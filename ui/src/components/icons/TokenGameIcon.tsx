c80c6e4b66d7ba81303561d37b1dabeabdb89099
Starknet Dev <starknetdev@gmail.com>
2025-04-10 12:50:19 +0100

    - remove unused settings images - make game token icons fixed

--- ui/src/components/icons/TokenGameIcon.tsx
+++ ui/src/components/icons/TokenGameIcon.tsx
@@ -11,20 +11,30 @@ const TokenGameIcon = ({
   size = "sm",
   tokenColor = "text-brand/25",
 }: TokenGameIconProps) => {
-  const sizeClasses = {
+  const tokenSizeClasses = {
     xs: "size-5 3xl:size-6",
     sm: "size-8 3xl:size-10",
     md: "size-10 3xl:size-12",
     lg: "size-16 3xl:size-20",
   };
+  const gameSizeClasses = {
+    xs: "size-4 3xl:size-5",
+    sm: "size-6 3xl:size-8",
+    md: "size-8 3xl:size-10",
+    lg: "size-12 3xl:size-16",
+  };
   return (
     <span className="relative inline-flex items-center justify-center">
-      <span className={`${tokenColor} ${sizeClasses[size]}`}>
+      <span className={`${tokenColor} ${tokenSizeClasses[size]}`}>
         <TOKEN />
       </span>
       <span className="absolute inset-0 flex items-center justify-center">
         {image ? (
-          <img src={image} alt="game-logo" className={`h-[50%] w-auto`} />
+          <img
+            src={image}
+            alt="game-logo"
+            className={`${gameSizeClasses[size]}`}
+          />
         ) : (
           <span className="w-full h-full">
             <QUESTION />
